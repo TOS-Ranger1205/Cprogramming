25978fb15ebe864f3f710c0dd2f90327d9fc0910
TOS-Ranger1205 <142795278+TOS-Ranger1205@users.noreply.github.com>
2023-08-23 11:30:32 +0000

    update Data

--- Main.c
+++ Main.c
@@ -9,19 +9,19 @@ int main(){
     data[0] = 23;
     data[1] = 90;
     data[2] = 8;
-    data[3] = 88;
-    data[4] = 88;
-    data[5] = 88;
-    data[6] = 88;
-    data[7] = 88;
-    data[8] = 88;
+    data[3] = 54;
+    data[4] = 65;
+    data[5] = 83;
+    data[6] = 25;
+    data[7] = 57;
+    data[8] = 52;
 
     // access data 
 
-    //printf("%d",data[1]);
 
     for (size_t i = 0; i < 9; i++)
     {
+      
         printf("%d\n",data[i]);
     }
     
