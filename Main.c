a7a5e27cd033b7b999b9a7dfd62cee68e6c5a432
TOS-Ranger1205 <142795278+TOS-Ranger1205@users.noreply.github.com>
2023-08-28 11:07:55 +0000

    Updata Data

--- Main.c
+++ Main.c
@@ -2,38 +2,19 @@
 
 int main(){
   // Array 1
-  int array1[6];
-
-    array1[0] = 12;
-    array1[1] = 23;
-    array1[2] = 34;
-    array1[3] = 45;
-    array1[4] = 56;
-    array1[5] = 67;
+  int array1[6] = {10,20,30,40,50,60};
 
   // Array 2
   
-  int array2[6];
-
-  array1[0] = 98;
-  array1[1] = 76;
-  array1[2] = 65;
-  array1[3] = 54;
-  array1[4] = 43;
-  array1[5] = 32;
+  int array2[6] ={10,20,30,40,50,60};
 
   int a;
-  int array3[a];
-
-if (a>0 , a<=6,a++)
+  
+for(a=0;a<6;a++)
 {
-  array3[a] = array1[a]+array2[a];
+  printf("Array3[%d]= %d\n",a,(array1[a]+array2[a]));
+  
 }
-
-if (a>0, a<7,a++)
-{
-  printf("Array3= %d\n " , array3[a]);
-}
-
+  
   return 0;
 }
