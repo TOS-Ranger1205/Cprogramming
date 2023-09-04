0c18fe7fe4d635d637b624cf2637dd2ef5ed6f9e
TOS-Ranger1205 <142795278+TOS-Ranger1205@users.noreply.github.com>
2023-09-04 10:18:21 +0000

    (❁´◡`❁)

--- Main.c
+++ Main.c
@@ -1,43 +1,18 @@
-// C Program to print first n terms of Fibonacci series using recursion
-// C Program to print the Fibonacci series using recursion
-#include <stdio.h>
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+  printf("Enter the number of elements:");    
+  scanf("%d",&number); 
+   
+ printf("\n%d %d",n1,n2);
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }    
 
-int prev1 = 1;
-int prev2 = 0;
-void fib(int n)
-{
-	if (n < 3) {
-		return;
-	}
-	int fn = prev1 + prev2;
-	prev2 = prev1;
-	prev1 = fn;
-	printf("%d ", fn);
-	return fib(n - 1);
-}
-void printFib(int n)
-{
-	if (n < 1) {
-		printf("Invalid number of terms\n");
-	}
-	else if (n == 1) {
-		printf("%d ", 0);
-	}
-	else if (n == 2) {
-		printf("%d %d", 0, 1);
-	}
-	else {
-		printf("%d %d ", 0, 1);
-		fib(n);
-	}
-	return;
-}
-
-int main()
-{
-	int n;
-  printf("Enter Your Number : ");
-  scanf("%d",&n);
-	printFib(n);
-	return 0;
-}
