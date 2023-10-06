b889165f1e6d85f7a03545d6a316c64b3b8c5ff0
TOS-Ranger1205 <142795278+TOS-Ranger1205@users.noreply.github.com>
2023-10-06 09:05:15 +0000

    Recursive

---
+++ RecursiveFun.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int ac(int );
+
+void main(){
+
+ 
+  ac(1);
+
+
+}
+
+int ac(int num){
+
+  if (num<=5)
+  {
+    printf("%d,",num);
+    num++;
+    
+    return ac(num);
+  }
+  else{
+    return 0;
+  }
+}
